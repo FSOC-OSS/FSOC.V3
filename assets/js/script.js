f9e1180061564c4e1411ed60d7fb1cc9a59acf05
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-09-17 15:20:29 +0530

    2nd

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,11 +42,8 @@ const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
   document.body.classList.toggle("nav-active");
 }
-
-addEventOnElements(navTogglers, "click", toggleNavbar);
 
 
 
